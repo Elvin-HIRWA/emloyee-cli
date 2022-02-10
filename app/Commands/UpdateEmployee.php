7cca9d375a8a887173089b3d1994787e4b0af41f
Elvin-HIRWA <elhirwa3@gmail.com>
2022-02-10 15:55:47 +0200

    adding functionality of Updating an Employee

--- app/Commands/UpdateEmployee.php
+++ app/Commands/UpdateEmployee.php
@@ -4,6 +4,7 @@ namespace App\Commands;
 
 use Illuminate\Console\Scheduling\Schedule;
 use LaravelZero\Framework\Commands\Command;
+use Illuminate\Support\Facades\DB;
 
 class UpdateEmployee extends Command
 {
@@ -12,14 +13,14 @@ class UpdateEmployee extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = '5 {id}';
 
     /**
      * The description of the command.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'To Update an Employe whose ID is Specified';
 
     /**
      * Execute the console command.
@@ -28,7 +29,32 @@ class UpdateEmployee extends Command
      */
     public function handle()
     {
-        //
+        $employee = DB::table('employee')->find($this->argument('id'));
+        if($employee){
+
+            $name = $this->ask('Update your Name');
+            if($name === null){
+                $this->warn("invalid name");
+                return;
+            }
+            $email = $this->ask('Update your Email');
+            if($email === null && filter_var($email, FILTER_VALIDATE_EMAIL) === false){
+                $this->warn("invalid email");
+                return;
+            }
+            $phone = $this->ask('Update your Phone number');
+            DB::table('employee')
+              ->where('id', $this->argument('id'))
+              ->update([
+                  'name' => $name,
+              'email' => $email,
+              'phone' => $phone
+            ]);
+            $this->notify('Notification','an Employee is Updated','Images/edit.jpg');  
+        }
+     else {
+         echo 'There is no employee with this ID';
+     }       
     }
 
     /**
